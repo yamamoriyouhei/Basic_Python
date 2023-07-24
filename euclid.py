68fb00ba9d09f0da721f9c0dbb0770669be961d4
yohei <yamamoriyouhei@gmail.com>
2023-07-24 15:27:30 +0900

    コース課題を解きました。

--- euclid.py
+++ euclid.py
@@ -2,6 +2,13 @@ a = int(input("a の値を入力: "))
 b = int(input("b の値を入力: "))
 
 # TODO
-while b != 0:
-    a, b = b, a%b
-print("aとbの最大公約数は",a)
+def disjoint(a, b):
+    while b != 0:
+        a, b = b, a % b
+
+    if a == 1:
+        print("a, bは互いに素です")
+    else:
+        print("a, bの最大公約数は", a)
+
+disjoint(a, b)
--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -1,17 +1,25 @@
+import math
+def trapezoidal_integral(f, a=0, b=1, n=100):
+    h = (b - a) / n
+    line = 0
+    for i in range(n + 1):
+        x = a + i * h
+        if i == 0 or i == n:
+            line += f(x)
+        else:
+            line  =  line + 2 * f(x)
+
+    line =  line * h / 2
+    return line
+
+result1 = trapezoidal_integral(math.sin, 0, math.pi/2, 100)
+print("(1) 関数 sin(x) の台形積分結果:", result1)
 
 import math
-a = 0
-b = math.pi/2
-n = 100
-h = (b - a) / n
-line = 0
-for i in range(n + 1):
-    x = a + i * h
-    if i == 0 or i == n:
-        line += math.sin(x)
-    else:
-        line  =  line + 2 * math.sin(x)
+result2 = trapezoidal_integral(lambda x:4/(1+ x**2), 0, 1, 100)
+print("(2) 関数 4/(1+x**2) の台形積分結果::", result2)
 
-line =  line * h / 2
+import math
+result3 = trapezoidal_integral(lambda x: math.sqrt(math.pi)*math.exp(-x**2), -100, 100, 1000)
+print("(3) 関数 √π*e^(-x^2) の台形積分結果:", result3)
 
-print("sin関数の台形積分の近似値は:", line)
